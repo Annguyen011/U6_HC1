e385540cea0879c2aa879e1943ee9fba65e2db0d
Annguyen011 <nguyenvanan01102003@gmail.com>
2024-03-13 15:17:08 +0700

    Finish fish

--- Assets/1_The Fish Master Assets/Scripts/Hook/Hook.cs
+++ Assets/1_The Fish Master Assets/Scripts/Hook/Hook.cs
@@ -1,84 +1,118 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
-namespace a_Fish
-{
-    [RequireComponent(typeof(Rigidbody2D))]
-    [RequireComponent(typeof(CircleCollider2D))]
-    public class Hook : MonoBehaviour
+public class Hook : MonoBehaviour {
+
+    public Transform hookedTransfrom;
+
+    private Camera mainCamera;
+    private Collider2D coll;
+
+    private int length;
+    private int strength;
+    private int fishCount;
+
+    private bool canMove;
+
+    private List<Fish> hookedFishes;
+
+    private Tweener cameraTween;
+
+	void Awake () {
+        mainCamera = Camera.main;
+        coll = GetComponent<Collider2D>();
+        hookedFishes = new List<Fish>();
+	}
+
+    void Update()
     {
-        #region [Variablies]
-
-        [Header("# Infos")]
-        public Transform hookedTranform;
-        private bool canMove = true;
-        private int length;
-        private int streength;
-        private int fishCount;
-
-        [Header("# Camera")]
-        [SerializeField] private Camera mainCamera;
-        private Vector3 startPos;
-
-
-        // Components
-        private Rigidbody2D rb;
-        private Collider2D coll;
-
-        #endregion
-
-        #region [Unity methods]
-
-        private void Awake()
+        if (canMove && Input.GetMouseButton(0))
         {
-            mainCamera = Camera.main;
-            coll = GetComponent<Collider2D>();
+            Vector3 vector = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 position = transform.position;
+            position.x = vector.x;
+            transform.position = position;
         }
+	}
 
-        private void Start()
+    public void StartFishing()
+    {
+        length = IdleManager.instance.length - 20;
+        strength = IdleManager.instance.strength;
+        fishCount = 0;
+        float time = (-length) * 0.1f;
+
+        cameraTween = mainCamera.transform.DOMoveY(length, 1 + time * 0.25f, false).OnUpdate(delegate
         {
-            startPos = transform.position;
-        }
-
-        private void Update()
+            if (mainCamera.transform.position.y <= -11)
+                transform.SetParent(mainCamera.transform);
+        }).OnComplete(delegate
         {
-            if (canMove && Input.GetMouseButtonDown(0))
-            {
-                Vector3 vector = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-                Vector3 pos = transform.position;
-
-                pos.x = vector.x;
-                transform.position = pos;
-            }
-        }
-
-        #endregion
-
-        #region [Override]
-
-        #endregion
-
-        public void StartFishing()
-        {
-            length = -50;
-            streength = 3;
-            fishCount = 0;
-            float time = (-length) * .1f;
-
-            coll.enabled = false;
-            canMove = true;
-
-            if(Vector3.Distance(startPos, transform.position) <= .3f)
-            {
-                StopFishing();
-            }
-        }
-
-        private void StopFishing()
-        {
-            canMove = false;
             coll.enabled = true;
+            cameraTween = mainCamera.transform.DOMoveY(0, time * 5, false).OnUpdate(delegate
+            {
+                if (mainCamera.transform.position.y >= -25f)
+                    StopFishing();
+            });
+        });
+
+        ScreensManager.instance.ChangeScreen(Screens.GAME);
+        coll.enabled = false;
+        canMove = true;
+        hookedFishes.Clear();
+
+
+    }
+
+    void StopFishing()
+    {
+        canMove = false;
+        cameraTween.Kill(false);
+        cameraTween = mainCamera.transform.DOMoveY(0, 2, false).OnUpdate(delegate
+        {
+            if (mainCamera.transform.position.y >= -11)
+            {
+                transform.SetParent(null);
+                transform.position = new Vector2(transform.position.x, -6);
+            }
+        }).OnComplete(delegate
+        {
+            transform.position = Vector2.down * 6;
+            coll.enabled = true;
+            int num = 0;
+            for(int i = 0; i < hookedFishes.Count; i++)
+            {
+                hookedFishes[i].transform.SetParent(null);
+                hookedFishes[i].ResetFish();
+                num += hookedFishes[i].Type.price;
+            }
+            IdleManager.instance.totalGain = num;
+            ScreensManager.instance.ChangeScreen(Screens.END);
+        });
+    }
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if(target.CompareTag("Fish") && fishCount != strength)
+        {
+            fishCount++;
+            Fish component = target.GetComponent<Fish>();
+            component.Hooked();
+            hookedFishes.Add(component);
+            target.transform.SetParent(transform);
+            target.transform.position = hookedTransfrom.position;
+            target.transform.rotation = hookedTransfrom.rotation;
+            target.transform.localScale = Vector3.one;
+
+            target.transform.DOShakeRotation(5, Vector3.forward * 45, 10, 90, false).SetLoops(1, LoopType.Yoyo).OnComplete(delegate
+            {
+                target.transform.rotation = Quaternion.identity;
+            });
+            if (fishCount == strength)
+                StopFishing();
         }
     }
+
 }
