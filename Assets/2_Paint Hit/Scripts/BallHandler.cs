8ab48e61b4ba1c0015439e6a430324ecfe90b726
Annguyen011 <nguyenvanan01102003@gmail.com>
2024-03-14 07:22:11 +0700

    Add scripts

--- Assets/2_Paint Hit/Scripts/BallHandler.cs
+++ Assets/2_Paint Hit/Scripts/BallHandler.cs
@@ -9,7 +9,8 @@ public class BallHandler : MonoBehaviour
     public GameObject ball;
 
     private float speed = 100;
-
+    private int circleNo;
+    [SerializeField] private int ballCount;
     private void Start()
     {
         MakeANewCircle();
@@ -25,6 +26,13 @@ public class BallHandler : MonoBehaviour
 
     private void HitBall()
     {
+        if (ballCount <= 1)
+        {
+            Invoke(nameof(MakeANewCircle), .1f);
+        }
+
+        ballCount--;
+
         GameObject gameObject = Instantiate<GameObject>(ball, new Vector3(0, 0, -8), Quaternion.identity);
         gameObject.GetComponent<MeshRenderer>().material.color = oneColor;
         gameObject.GetComponent<Rigidbody>().AddForce(Vector3.forward * speed, ForceMode.Force);
@@ -32,6 +40,8 @@ public class BallHandler : MonoBehaviour
 
     private void MakeANewCircle()
     {
+        GameObject[] array = GameObject.FindGameObjectsWithTag("circle");
+
         GameObject gameObject = Instantiate(Resources.Load("round" + UnityEngine.Random.Range(1, 4))) as GameObject;
         gameObject.transform.position = Vector3.forward * 23;
         gameObject.name = "Circle";
